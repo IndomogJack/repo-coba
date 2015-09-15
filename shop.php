f4dae9a36e633275a40822f1f3d687930c3203cc
Joko Susilo <joko.susilo@indomog.com>
2015-09-15 14:28:09 +0700

    update file

---
+++ shop.php
@@ -0,0 +1,348 @@
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Indomog Warnet</title>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="js/jquery-1.11.0.min.js"></script>
+<!-- Custom Theme files -->
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<!-- <link rel="stylesheet" href="css/main.css" type="text/css" media="all"> -->
+<!-- <script type="text/javascript" src="js/mootools.js"></script> -->
+<!-- <script type="text/javascript" src="js/sortableTable.js"></script> -->
+<!-- <link href="css/sortableTable.css" rel="stylesheet" type="text/css" /> -->
+<!-- Custom Theme files -->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<!-- Google Fonts -->
+<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500,700' rel='stylesheet' type='text/css'>
+     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script src="js/typeahead.min.js"></script>
+    <script>
+    $(document).ready(function(){
+    $('input.typeahead').typeahead({
+        name: 'typeahead',
+        remote:'search.php?key=%QUERY',
+        limit : 10
+    });
+});
+    </script>
+</head>
+<body>
+<!-- Header Starts Here -->
+<div class="header">
+	<div class="container">
+		<div class="logo">
+			<a href="index.html"><img src="images/logo_warnet.png" alt=""></a>
+		</div>
+		<span class="menu"></span>
+		<div class="navigation">
+			<ul class="navig cl-effect-3" >
+				<li><a href="index.php">Home</a></li>
+				<li><a href="shoping.php">Shop</a></li>
+				<li><a href="shoping.php">Daftar Produk</a></li>
+				<li><a href="topup.php">Top Up</a></li>
+				<li><a href="mutasi.mutasi">Mutasi</a></li>
+			</ul>
+			<div class="account-bar">
+			JOKO.SUSILO@INDOMOG.COM<br>
+			<small>SALDO : RP. 3000000,-</small>
+			<a href="#">LOGOUT</a>
+			</div>
+			<!-- <div class="logout-bar">
+			Logout
+			</div> -->
+			<script>
+				$( "span.menu" ).click(function() {
+				  $( ".navigation" ).slideToggle( "slow", function() {
+				    // Animation complete.
+				  });
+				});
+			</script>
+			<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!-- Header Ends Here -->	
+<!-- Games Page Starts here -->
+<div class="games">
+	<div class="container">
+		<div class="page-path">
+			<ul class="path-list">
+				<li><a href="index.html">Home</a></li>&nbsp;&nbsp;/&nbsp;&nbsp;
+				<li class="act">Shoping</li>
+			</ul>
+			<p><a href="index.html">Back to Home</a></p>
+			<div class="clearfix"></div>
+		</div>
+		<!-- <h3 class="page-header">
+			Article
+		</h3> -->
+		<div class="blog-content">
+			<div class="blog-post">
+				<h3><a href="single.html">Hot Items</a></h3>
+				<div class="label-caption">	
+				Informasi : Demi keamanan account Anda, kami sarankan kepada Anda agar rajin untuk melakukan pergantian password dan PIN secara berkala.
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<div class="shoping">
+				<img src="images/shop.jpg">
+				<h5><strong>Google Play</strong></h5>
+				<h5>Rp. 1.000.000</h5>
+				<a href="#">BELI</a><br>
+				<a href="#">BELI CEPAT</a>
+				</div>
+				<br><br>
+			</div>
+			<div class="blog-post">
+			<hr>
+			<h3>Pencarian Voucher!</h3>
+			<!-- <div class="bs-example"> -->
+        	<table><tr><td>
+        	<form id="post">
+        	<input type="text" name="post" class="typeahead tt-query" autocomplete="off" spellcheck="false" placeholder="Ketik Produk">
+        	</td><td>
+        	<select name="kategori"  class="select-search">
+        	<option value="1">3</option>
+        	<option value="2">Axis</option>
+        	<option value="3">Indosat</option>
+        	<option value="4">Telkomsel</option>
+        	</select></td>
+        	<td><input type="submit" name="submit" value="submit" id="submit" class="btn btn-success">
+        	</form>
+        	<script>
+			$('#submit').click(function(event) {
+			    event.preventDefault(); /*  Stops default form submit on click */       
+			    var post = $('#post').val();
+			    $.ajax({                                                                    
+			        url: "post.php?",
+			        data: 'post=' + post,
+			        type: "POST",
+			        success: function(data){
+			        $('#content').empty();
+			        $('#content').load('post.php');
+			        }
+			    });
+			});
+        	</script>
+        	</td>
+        	</tr></table>
+   			<!-- </div> -->
+			<div class="blog-post">
+				<p>
+				<div id="content"></div>
+				</p>
+				
+			</div>
+			</div>
+			</div>
+		<div class="blog-sidebar">
+			<h3 class="page-header">Cart</h3>
+			<div class="post-sidebar">
+			Informasi : Jangan Refresh browser saat transaksi sedang berlangsung.
+			<hr>
+			<img src="images/cart.png" align="left">
+			<b>Your Chart is Empty</b>
+			<br>
+			<br>
+			<br>
+
+			</div>
+			<div class="post-sidebar">
+			Saldo. Rp. 0  [<a href="#">Top Up</a>]
+			<br>
+			<a href="#">(+) Lihat Poin </a><br>
+			<a href="#">(+) Lihat Transaksi Terakhir </a><br>
+			<a href="#">(+) Update Harga </a><br>
+			<hr>
+			</div>
+			<h3 class="page-header">Support CS</h3>
+			<div class="post-sidebar">
+			Indomog CS ONe <br>
+			Indomog CS Two <br>
+			<img src="images/sys.png"><br><br><br>
+			HOT LINE : +62-21 2932-6161 (24 hours)<br>
+			FAX : +62-21 5292-0978<br>
+			EMAIL : support@indomog.com<br>
+			<hr>
+			</div>
+			<h3 class="page-header">KETERANGAN</h3>
+			<div class="post-sidebar">
+			 <ol>
+                <li>BELI = Untuk beli lebih dari 1 voucher.</li>
+                <li>BELI CEPAT = Untuk beli 1 voucher saja. </li>
+                <li>Jika baru mengisi saldo (TOP UP), harap refresh browser Anda. </li>
+                <li>Harap mencantumkan subjek : <b>POS BETA</b> di pesan YM Anda atau email Anda.</li>
+                <li>Klik di sini untuk download <a href="#">FAQ</a></li>
+                <li>Klik di sini untuk download <a href="#">User Manual</a></li>
+             </ol>
+			</div>
+
+		</div>
+		<div class="clearfix"></div>
+		
+	</div>
+</div>
+<div class="footer">
+	<div class="container">
+		<div class="col-md-3">
+			<ul class="social">
+				<li><i class="fa"></i></li>
+				<li><i class="fb"></i></li>
+				<li><i class="fc"></i></li>
+			</ul>
+			<small>Copyright </small>
+		</div>
+		<div class="col-md-3">
+		<small>
+		 <ul>
+                <li>Agate - Football Saga</li>
+                <li>Ameba - Pico World </li>
+                <li>Asia Soft - AIKA </li>
+                <li>Boyaa - Poker Texas Hold & Perjuangan Semut</li>
+                <li>CJ Indonesia - Elsword Online Indonesia</li>
+                <li>Coalaa - Texas Poker Pro.ID</li>
+                <li>Digital Eight - Giga Slave </li>
+                <li>Faveo - Dynasty War & Free Jack </li>
+                <li>Funnel - Bubble Ninja </li>
+                <li>Garena - HON & League of Legends</li>
+                <li>Geewa - Pool Live Tour </li>
+                <li>IAH Games - Dragonica & FIFA Online 2</li>
+                <li>IGG - GALAXY II</li>
+                <li>Joombi - Klab Kartu & Boom 3D</li>
+               
+                </ul>
+		</small>
+		</div>
+		<div class="col-md-3">
+		<small>
+		<ul>
+		 <li>Mobius - Living After War, Star Empires,
+                King of Heroes & Winning Goal </li>
+                <li>Olleh4U - Florensia & Dark Eden</li>
+                <li>Onsof - 99 Domino </li>
+                <li>Orange Game - Crystal Epic, Dragon Saga,
+                Rich State, Mahadewa Online & Dekaron</li>
+                <li>Playmojo - Fly For Fun, Legend of Lunia & 
+                Mikro Volts Reloaded</li>
+                <li>Qeon Interactive - Heva Online & Shadow 
+                Company</li>
+                <li>Softnyx - Gunbound 2, Love Beat & Wolf Team</li>
+                <li>Stardoll Media - Stardoll</li>
+                <li>Touchten - TOUCH JAM </li>
+                <li>VMG - Indogame</li>
+                
+                </ul>
+		</small>
+		</div>
+		<div class="col-md-3">
+		<small>
+		<ul>
+		<li>VTC Online - Gokong Online, Mercenary Ops,
+                Ran Returns</li>
+                <li>Winner - ACE Online, Bubble Ninja, Eden Online,
+                Grand Fantasia, Rosh Online & X Shot</li>
+                </ul>
+                <br>
+                <h6>Musik</h6>
+                <ul>
+                <li>Melon</li>
+                <li>Klik Musik</li>
+                </ul>
+                <br>
+                <h6>eBook</h6>
+                <ul>
+                <li>ngomik.com</li>
+                <li>wayang Force</li>
+                </ul>
+		</small>
+		</div>
+	</div>
+	
+</div>
+<!-- Footer Ends Here -->
+</body>
+</html>
