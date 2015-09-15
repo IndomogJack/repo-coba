2b5c640116cf8155d5482203184ae19bd92add2f
Joko Susilo <joko.susilo@indomog.com>
2015-09-15 15:20:48 +0700

    edit isi

--- tambah 2.php
+++ tambah 2.php
@@ -1,10 +1,5 @@
 <?php
-$db_username 	= 'root';
-$db_password 	= '';
-$db_name 		= 'filter';
-$db_host 		= 'localhost';
-$conn = mysql_connect($db_host, $db_username, $db_password) or die ('Error connecting to mysql');
-mysql_select_db($db_name) or die ('Unable to select  database!');
-$mysqli = new mysqli($db_host, $db_username, $db_password,$db_name);
+
+echo "nyoba ngedit isi";
 
 ?>
